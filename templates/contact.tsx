de2cf1a80f7bffcc1d8aff7445ceaac97c82c608
arifzakuan <arifzakuanbinadnan@airasia.com>
2024-01-05 00:52:07 +0800

    mb-300

--- templates/contact.tsx
+++ templates/contact.tsx
@@ -15,7 +15,7 @@ export default function ContactSection() {
 
   return (
     <Fade in={true} timeout={1000}>
-      <main className={'flex flex-row space-x-3 items-center mb-[200px] md:mb-36 mx-10 md:mx-80'}>
+      <main className={'flex flex-row space-x-3 items-center mb-[300px] md:mb-36 mx-10 md:mx-80'}>
         <div className='flex flex-row space-x-3 text-[20px] md:text-[30px]'>
           <a href="https://github.com/tehcanai" className='hover:text-white'>Github</a>
           <a href="https://www.linkedin.com/in/arif-zakuan-99460820b/" className='hover:text-white'>LinkedIn</a>
